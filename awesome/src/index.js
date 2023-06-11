fa5afcad88f07ba7d89233ee90c344452b3d089f
Alexander <alexanderpastfast@gmail.com>
2023-06-11 12:42:25 +0300

    110623

--- awesome/src/index.js
+++ awesome/src/index.js
@@ -4,18 +4,18 @@ import store from "./redux/redux-store.js";
 // import * as ReactDOM from "react-dom/client";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     {/* создаем контекст где <Provider/>- это родительский компонент, а внутри его дети(consumers) могут получить доступ к store без прокидывания по всей ветке */}
     <Provider store={store}>
       <React.StrictMode>
         <App />
       </React.StrictMode>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
